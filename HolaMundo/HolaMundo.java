6f488aa3f88de56cdab5b8290ee77c441a5b729e
Demonassit <demonassit@hotmail.com>
2020-10-15 16:30:39 -0500

    HolaMundo

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,35 @@
+class HolaMundo{
+    //para poder hacer un comentario sobre la linea de codigo
+    /*
+    para poder realizar comentarios
+    en diferentes lines de codigo
+    y asi escribir todo lo que querramos y no afectara al codigo
+    principal
+    
+    dos tipos de programadores
+
+    class nombreclase{
+
+    }
+
+    class nombreclase
+    {
+        //es el cuerpo del programa, aqui van todas las instrucciones
+        operaciones, metodos, varibles etc
+    }
+
+    */ 
+
+    public static void main(String[] args){
+        //una declaracion, una idea??, orden 
+        //estructura base de cualquiera clase principal
+
+        //inicializacion de varibales
+        String nombre = "Cosita kawaii";
+        //escribir "la oracion"
+        //clase.objeto.metodo
+        System.out.println("Por fis di Hola mundo");
+        System.out.println(nombre);
+    }
+
+}
