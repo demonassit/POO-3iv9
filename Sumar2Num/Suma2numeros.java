5076897f8e4ab00cbb88f1508618929cd3b92b25
Demonassit <demonassit@hotmail.com>
2020-10-15 17:41:16 -0500

    como caiga ¬¬

--- Sumar2Num/Suma2numeros.java
+++ Sumar2Num/Suma2numeros.java
@@ -6,26 +6,36 @@ class Suma2numeros{
     public static void main(String[] args){
         /*
         Vamos a crear un programa que se encargue de sumar dos numero
-        y como tarea  que sume n numeros wiiiii    >:v
+        y como tarea  que sume n numeros wiiiii que todos sean positivos 
+        y sino que imprima mensaje "numero negativo no se acepta"
+        y vuelva a pedir un numero positivo   >:v
         */ 
 
         //objeto para poder leer los numeros
         Scanner entrada = new Scanner(System.in);
 
         //variables
-        int num1, num2, resultado;
-
-
+        int num1; 
         
+        int resultado=0;
+
+        do{
+            //hacer la accion
+            System.out.println("Ingresa el numero a sumar: ");
+            num1 = entrada.nextInt();  //leer el siguiente entero
+            resultado = resultado + num1;
+        }while(num1!=0);
+
+        // mientras no sea cero   
+       
 
         //instrucciones del programa
-        System.out.println("Ingresa el primer numero a sumar: ");
-        num1 = entrada.nextInt();  //leer el siguiente entero
-        System.out.println("Ingresa el segundo numero a sumar: ");
-        num2 = entrada.nextInt();  //leer el siguiente entero
+       
+        //System.out.println("Ingresa el segundo numero a sumar: ");
+        //num2 = entrada.nextInt();  //leer el siguiente entero
 
         //operacion
-        resultado = num1 + num2;
+        //resultado = num1 + num2;
 
         System.out.println("La suma de los numeros es de: "+resultado);
 
