8c714fd747b6b7c01ed1729bdbffa8aad53f8384
Demonassit <demonassit@hotmail.com>
2020-10-15 16:51:13 -0500

    programa de entrada de datos

---
+++ EntradaDatos/EntradaDatos.java
@@ -0,0 +1,14 @@
+class EntradaDatos{
+
+    /* Este programa nos va a servir para 
+    poder meter datos*/
+
+    public static void main(String[] args){
+        String nombre;
+        System.out.println("Dime tu nombre nwn/ ");
+        //leer
+        //entrada estandar de la computadora
+        nombre = System.console().readLine();
+        System.out.println("Biendenivo : "+nombre+" a programacion");
+    }
+}
