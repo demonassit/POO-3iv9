a7d69a7b1bf89d16dc17ce3ef82472ea4f729302
Demonassit <demonassit@hotmail.com>
2020-11-13 16:42:41 -0600

    cambios en main perro gato

---
+++ Herencia/main.java
@@ -0,0 +1,28 @@
+/*
+Esta va a ser la clase principal que hara las invocaciones
+de los objetos y metodos
+
+*/
+
+
+public class main{
+
+    //metodo principal
+    public static void main(String[] args){
+
+        //y por ahora es todo
+
+        //necesito los objetos de clada una de las clases
+        //de aqui mismo puedo pasar los parametros 
+        Perro dog = new Perro("Pulgas", "Croquetas", 1, "Pastor aleman");
+        Gato cat = new Gato("Gato", "anvorgueza", 2, 9);
+
+        //mostrar los datos
+
+        dog.mostrar();
+        System.out.println("uwu");
+        cat.mostrar();
+        System.out.println("ono");
+
+    }
+}
